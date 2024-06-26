09084077a48b71c0c135f44b0997612e46c77fcc
Gabriella Massaro <gmassaro807@gmail.com>
2024-06-26 11:59:56 -0400

    Create search by description/name

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,8 @@ import ListingsContainer from "./ListingsContainer"
 
 function App() {
   const [listings, setListings] = useState([])
+  const [search, setSearch] = useState("")
+
   useEffect(() => {
     fetch("http://localhost:6001/listings")
       .then((response) => response.json())
@@ -14,10 +16,20 @@ function App() {
     const remainingListings = listings.filter((listing) => listing.id !== id)
     setListings(remainingListings)
   }
+
+  const filterListings = listings.filter((listing) => {
+    const listingDescription = listing.description.toLowerCase()
+
+    return listingDescription.startsWith(search.toLowerCase())
+  })
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer listings={listings} onHandleDelete={handleDelete} />
+      <Header search={search} setSearch={setSearch} />
+      <ListingsContainer
+        listings={filterListings}
+        onHandleDelete={handleDelete}
+      />
     </div>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
-import React from "react";
-import Search from "./Search";
+import React from "react"
+import Search from "./Search"
 
-function Header() {
+function Header({ search, setSearch }) {
   return (
     <header>
       <h1>
@@ -10,9 +10,9 @@ function Header() {
         </span>
         gregslist
       </h1>
-      <Search />
+      <Search search={search} setSearch={setSearch} />
     </header>
-  );
+  )
 }
 
-export default Header;
+export default Header
